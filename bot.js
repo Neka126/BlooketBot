35c5c57da737250f849f5476350b359f235a62e4
Frostyanimations126 <39423395+Frostyanimations126@users.noreply.github.com>
2021-05-10 11:43:18 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,5 +2,5 @@ var jq = document.createElement('script');
 jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
 var bbc = document.createElement('script');
-bbc.src = "https://rawcdn.githack.com/Neka126/BlooketBot/20d3061e5b25112d0421cceae802e78204003904/bot.js";
+bbc.src = "https://rawcdn.githack.com/Neka126/BlooketBot/b4b9204588805c0ca35d198e814bf301758ffd97/index.js";
 document.getElementsByTagName('head')[0].appendChild(bbc);
