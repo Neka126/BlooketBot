20d3061e5b25112d0421cceae802e78204003904
Frostyanimations126 <39423395+Frostyanimations126@users.noreply.github.com>
2021-05-10 11:28:28 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,6 @@
+var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+var bbc = document.createElement('script');
+bbc.src = "https://rawcdn.githack.com/Neka126/BlooketBot/ceff36b30c935789c07d5fe25eb3812994763e26/index.js";
+document.getElementsByTagName('head')[0].appendChild(bbc);
